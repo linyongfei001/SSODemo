2633599e00980a9d9c87e54b9dae6e50f409d9f8
linyongfei001 <1451500795@qq.com>
2019-06-05 15:42:19 +0800

    v1.0.1

--- src/main/java/utils/MyArraysUtils.java
+++ src/main/java/utils/MyArraysUtils.java
@@ -6,15 +6,24 @@ import java.util.Random;
 public class MyArraysUtils {
 
     /**
-     * 生成一个数组
+     * 生成一个String类型数组
      * @param length，数组的长度
      * @return，返回一个String类型的数组
      */
     public String[] addArray(int length){
         String [] arr = new String[length];
         Random r = new Random(100);
-        for (int i=0;i<=length;i++){
+        for (int i=0;i< length;i++){
             arr[i] = r.nextInt(100)+"";
+        }
+        return arr;
+    }
+
+    public int [] addIntArray(int length){
+        int [] arr = new int[length];
+        Random r = new Random(100);
+        for (int i=0;i< length;i++){
+            arr[i] = r.nextInt(100);
         }
         return arr;
     }
@@ -27,6 +36,14 @@ public class MyArraysUtils {
         for(int i =0;i<arr.length;i++){
             System.out.print(arr[i]+"\t");
         }
+        System.out.println();
+    }
+
+    public void showArr(int [] arr){
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i]+"\t");
+        }
+        System.out.println();
     }
 
 }
